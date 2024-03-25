fa56072708449222eaf24ffb226e3ace82e1c0ab
nicolebass7 <nicole.bass@eagles.oc.edu>
2024-03-25 13:56:32 -0500

    import viewEditAsset.vue

--- src/router.js
+++ src/router.js
@@ -15,6 +15,7 @@ import PersonHistory from "./views/PersonHistory.vue";
 import PersonList from "./views/PersonList.vue";
 
 import checkoutAsset from "./views/checkoutAsset.vue";
+import ViewEditAsset from "./views/ViewEditAsset.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
